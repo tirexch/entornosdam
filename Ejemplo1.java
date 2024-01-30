74c86d0e4c02a87f320b7269c867efbe98a4f15c
dam23-19 <dam23-19@c16x11.iescierva.net>
2024-01-30 09:27:10 +0100

    EDT2 Mostrar min umbre en un mensaje

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -1,5 +1,7 @@
 public class Ejemplo1 {
    public static void main (String[] args) {
         System.out.println("Hola mundo");
+        String nombre = "Tirex";
+        System.out.println("Mi nimbre es" + nombre);
    } 
 }
